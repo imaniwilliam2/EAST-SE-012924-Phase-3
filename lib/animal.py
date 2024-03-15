0c7e546e813bd9c8e69711a348b8741678b2c639
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-03-15 12:08:29 -0400

    lecture complete

--- lib/animal.py
+++ lib/animal.py
@@ -1,4 +1,46 @@
 import ipdb
 
 class Animal:
-    pass
+
+    all = []
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age 
+
+        Animal.all.append(self)
+
+    def make_animal_sound(self):
+        print("Animal Sound!")
+
+
+class Dog(Animal):
+
+    all = []
+
+    def __init__(self, name, age, odedience_level = 3, bark_volume = 3):
+        super().__init__(name, age)
+        self.obedience_level = odedience_level
+        self.bark_volume = bark_volume
+
+        Dog.all.append(self)
+    
+    def make_animal_sound(self):
+        exclamation = "!" * self.bark_volume
+        print(f"Bark{exclamation}")
+    
+
+class Cat(Animal):
+
+    all = []
+
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        Cat.all.append(self)
+
+    def make_animal_sound(self):
+        print("Meow!")
+
+
+ipdb.set_trace()
